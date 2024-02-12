6bbfae710202c50c7764045304b97b3d04e97d25
FalkDK <35104340+FalkDK@users.noreply.github.com>
2024-02-12 12:20:43 +0100

    changed layout for top50 isin

--- streamlit_app.py
+++ streamlit_app.py
@@ -174,135 +174,148 @@ def gather_loan_shares(df, selected_isins):
     shares_df = pd.DataFrame(shares_data)
     return shares_df.set_index('isin')
 
+#
+# Main application with page navigation using st.radio
+def main():
+    st.set_page_config(layout="centered", page_title='Callable Analysis')
+    st.title("Danish Bonds Analysis")
 
-# Main Streamlit app
-st.set_page_config(layout="centered", page_title='Callables')  # wide/centered
-st.title("Debitor Distribution")
+    # sidebar
+    with st.sidebar:
+        st.button('Debitor')
+        st.button('Cash Flow')
+        
+    df = load_data()
+    
+    # default isin
+    default_isins = ['DK0009540981', 'DK0009409922', 'DK0006359286', 'DK0004626918', 'DK0002058346']
+    isin_options = df['isin'].unique()
 
+    # Ensure all default ISINs are in the options
+    default_isins = [isin for isin in default_isins if isin in isin_options]
+    selected_isins = st.multiselect("Select ISINs:", options=isin_options, default=default_isins)
 
-# Load data
-df = load_data()
+    # Define the page navigation
+    page = st.radio("Navigate to 👇", ["Full Table", "List Large Loans"], horizontal=True)
+    st.subheader('', divider='grey')
 
-# default isin
-default_isins = ['DK0009540981', 'DK0009409922', 'DK0006359286', 'DK0004626918', 'DK0002058346']
-isin_options = df['isin'].unique()
+    if page == "Full Table":
+        display_main_analysis(df, selected_isins)
+    elif page == "List Large Loans":
+        display_top_20_isins(df, isin_options)
 
-# Ensure all default ISINs are in the options
-default_isins = [isin for isin in default_isins if isin in isin_options]
-selected_isins = st.multiselect("Select ISINs:", options=isin_options, default=default_isins)
+def display_main_analysis(df, selected_isins):
+    # Your existing logic for displaying raw data, debtor distribution, loan sizes, etc.
+    st.title("Debitor Distribution")
+    
+     #Checkbox to show/hide raw data
+    show_raw_data = st.checkbox('Show Raw Data')
+    #show_debitor_table = st.checkbox('Show Tables')
 
-# sidebar
-with st.sidebar:
-    st.button('Debitor')
-    st.button('Cash Flow')
+    # Show raw data
+    if show_raw_data:
+        st.subheader('Raw Data')
+        if selected_isins:
+            for isin in selected_isins:
+                st.write(f"ISIN: {isin}")
+                st.table(df[(df['isin'] == isin)].sort_values(by=['laan_gruppe', 'restgaeldinterval']))
+        else:
+            st.write("Please select at least one ISIN.")
 
-# Checkbox to show/hide raw data
-show_raw_data = st.checkbox('Show Raw Data')
-show_debitor_table = st.checkbox('Show Tables')
-
-# Show raw data
-if show_raw_data:
-    st.subheader('Raw Data')
+    # Show debtor distribution as percentages
     if selected_isins:
+        st.subheader('Debitor Distribution (%)', divider='red')
+        percentage_df = calculate_percentage(df, selected_isins)
+        pivoted_df = percentage_df.pivot_table(index=['isin', 'restgaeldinterval'], columns='laan_gruppe', values='percentage', aggfunc='sum', margins=True, margins_name="Total")
+        pivoted_df_kontant = percentage_df.pivot_table(index=['isin', 'restgaeldinterval'], columns='laan_gruppe', values='percentage_obl_kontant', aggfunc='sum', margins=True, margins_name="Total")
+
         for isin in selected_isins:
-            st.write(f"ISIN: {isin}")
-            st.table(df[(df['isin'] == isin)].sort_values(by=['laan_gruppe', 'restgaeldinterval']))
+            rest_obl = df[(df['isin'] == isin)]['restgaeld_obl'].sum()
+            rest_kontant = df[(df['isin'] == isin)]['restgaeld_obl_kontant'].sum()
+            share_obl, share_kontant = calculate_restgaeld_shares(df, isin)
+            share_a, share_b = calculate_loan_shares(df, isin)
+            st.write(f"**ISIN:** {isin} | **Private Share:** {round(share_a, 3)} | **Commercial Share:** {round(share_b,3)}")
+            col1, col2 = st.columns(2)
+            with col1:
+                st.write(f'**Obl:** {rest_obl:,} | **Share:** {round(share_obl, 3)}')
+                st.table(pivoted_df.loc[isin].fillna(0))  # Using fillna(0) to replace NaN values with 0 for better presentation
+            with col2:
+                st.write(f'**Cash:** {rest_kontant:,} | **Share:** {round(share_kontant, 3)} ')
+                st.table(pivoted_df_kontant.loc[isin].fillna(0))  # Using fillna(0) to replace NaN values with 0 for better presentation
+
+        # Plotting the combined bar chart for debtor distribution
+        st.subheader('Debitor Distribution (%)', divider='red')
+        fig = px.bar(percentage_df, x='restgaeldinterval', y='percentage', color='isin', barmode='group', title='Percentage Distribution of Obl')
+        st.plotly_chart(fig)
+        fig = px.bar(percentage_df, x='restgaeldinterval', y='percentage_obl_kontant', color='isin', barmode='group', title='Percentage Distribution of Cash Loans')
+        st.plotly_chart(fig)
+        
+    # Show average loan size
+    if selected_isins:
+        st.subheader('Avg Loan Size', divider='red')
+        show_full = st.checkbox('Full Breakdown')
+        for isin in selected_isins:
+            st.write(f"**ISIN:** {isin}")
+            loan_size_df = compute_avg_loan_size(df, [isin])
+            loan_size_df_gruppe = calculate_avg_loan_size_per_laan_gruppe(df, [isin])
+            if not show_full:
+                st.table(loan_size_df_gruppe.pivot(index='restgaeldinterval', columns='laan_gruppe', values='avg_loan_size').fillna(0))
+            else:
+                st.table(loan_size_df.pivot_table(index='restgaeldinterval', columns='laan_gruppe', values=['Avg_obl_loan', 'Avg_cash_loan'], aggfunc='mean').fillna(0))
+
+        # Plotting the combined bar chart for average loan sizes
+        st.subheader('Avg Loan Size - Combined Bar Chart', divider='red')
+        loan_size_df_combined = compute_avg_loan_size(df, selected_isins)
+        fig_loan_size = px.bar(loan_size_df_combined.melt(id_vars=['isin', 'restgaeldinterval'], value_vars=['Avg_obl_loan', 'Avg_cash_loan']), 
+                                x='restgaeldinterval', y='value', color='isin', facet_col='variable', barmode='group',
+                                title='Average Loan Sizes Across Restgaeldinterval for Obl and Cash loans')
+        
+        st.plotly_chart(fig_loan_size)
+        
+        # Plotting across interval per gruppe
+        loan_size_df_gruppe_combined = calculate_avg_loan_size_per_laan_gruppe(df, selected_isins)
+        fig_loan_size = px.bar(
+            loan_size_df_gruppe_combined.melt(id_vars=['isin', 'restgaeldinterval', 'laan_gruppe'], value_vars=['avg_loan_size']),
+            x='restgaeldinterval',
+            y='value',
+            color='isin',
+            facet_col='laan_gruppe',
+            barmode='group',
+            title='Average Loan Sizes Across Restgaeldinterval for Privat and Other Loans'
+        )
+
+        st.plotly_chart(fig_loan_size)
+
+    # Calculate interval distribution and store in a variable
+    interval_distribution_df = calculate_interval_distribution(df, selected_isins)
+    interval_distribution_df.columns = ["0-200k", '200k-500k', '500k-1m', '1-3m', '3-10m', '10-50m', '+50m']
+
+    # Calculate shares and store in a variable
+    loan_shares_df = gather_loan_shares(df, selected_isins)
+
+    # Merge the two DataFrames side by side
+    merged_df = interval_distribution_df.merge(loan_shares_df, left_index=True, right_index=True)
+    merged_df.columns = merged_df.columns.map(str)
+
+    # Display the merged DataFrame using Streamlit
+    if selected_isins:
+        st.subheader('QuickView per ISIN')
+        st.dataframe(merged_df)
+
     else:
         st.write("Please select at least one ISIN.")
 
-# Show debtor distribution as percentages
-if selected_isins:
-    st.subheader('Debitor Distribution (%)', divider='red')
-    percentage_df = calculate_percentage(df, selected_isins)
-    pivoted_df = percentage_df.pivot_table(index=['isin', 'restgaeldinterval'], columns='laan_gruppe', values='percentage', aggfunc='sum', margins=True, margins_name="Total")
-    pivoted_df_kontant = percentage_df.pivot_table(index=['isin', 'restgaeldinterval'], columns='laan_gruppe', values='percentage_obl_kontant', aggfunc='sum', margins=True, margins_name="Total")
-
-    for isin in selected_isins:
-        rest_obl = df[(df['isin'] == isin)]['restgaeld_obl'].sum()
-        rest_kontant = df[(df['isin'] == isin)]['restgaeld_obl_kontant'].sum()
-        share_obl, share_kontant = calculate_restgaeld_shares(df, isin)
-        share_a, share_b = calculate_loan_shares(df, isin)
-        st.write(f"**ISIN:** {isin} | **Private Share:** {round(share_a, 3)} | **Commercial Share:** {round(share_b,3)}")
-        col1, col2 = st.columns(2)
-        with col1:
-            st.write(f'**Obl:** {rest_obl:,} | **Share:** {round(share_obl, 3)}')
-            st.table(pivoted_df.loc[isin].fillna(0))  # Using fillna(0) to replace NaN values with 0 for better presentation
-        with col2:
-            st.write(f'**Cash:** {rest_kontant:,} | **Share:** {round(share_kontant, 3)} ')
-            st.table(pivoted_df_kontant.loc[isin].fillna(0))  # Using fillna(0) to replace NaN values with 0 for better presentation
-
-    # Plotting the combined bar chart for debtor distribution
-    st.subheader('Debitor Distribution (%)', divider='red')
-    fig = px.bar(percentage_df, x='restgaeldinterval', y='percentage', color='isin', barmode='group', title='Percentage Distribution of Restgaeld_obl Across Restgaeldinterval')
-    st.plotly_chart(fig)
-    fig = px.bar(percentage_df, x='restgaeldinterval', y='percentage_obl_kontant', color='isin', barmode='group', title='Percentage Distribution of Restgaeld_obl_kontant')
-    st.plotly_chart(fig)
+def display_top_20_isins(df, isin_options):
+    total_int_distribution = calculate_interval_distribution(df, isin_options)
+    total_int_distribution.columns = ["0-200k", '200k-500k', '500k-1m', '1-3m', '3-10m', '10-50m', '+50m']
+    total_loan_shares_df = gather_loan_shares(df, isin_options)
+    total_merged_df = total_int_distribution.merge(total_loan_shares_df, left_index=True, right_index=True)
+    total_merged_df.columns = total_merged_df.columns.map(str)
     
-# Show average loan size
-if selected_isins:
-    st.subheader('Avg Loan Size', divider='red')
-    show_full = st.checkbox('Full Breakdown')
-    for isin in selected_isins:
-        st.write(f"ISIN: {isin}")
-        loan_size_df = compute_avg_loan_size(df, [isin])
-        loan_size_df_gruppe = calculate_avg_loan_size_per_laan_gruppe(df, [isin])
-        if not show_full:
-            st.table(loan_size_df_gruppe.pivot(index='restgaeldinterval', columns='laan_gruppe', values='avg_loan_size').fillna(0))
-        else:
-            st.table(loan_size_df.pivot_table(index='restgaeldinterval', columns='laan_gruppe', values=['Avg_obl_loan', 'Avg_cash_loan'], aggfunc='mean').fillna(0))
+    top_20_isins_for_50m = total_merged_df.sort_values('+50m', ascending=False).head(50)
+    st.subheader("Top 50 ISINs with the Highest Percentage in '+50m'")
+    st.dataframe(top_20_isins_for_50m)
+    # Optional: Add visualization here
 
-    # Plotting the combined bar chart for average loan sizes
-    st.subheader('Avg Loan Size - Combined Bar Chart', divider='red')
-    loan_size_df_combined = compute_avg_loan_size(df, selected_isins)
-    fig_loan_size = px.bar(loan_size_df_combined.melt(id_vars=['isin', 'restgaeldinterval'], value_vars=['Avg_obl_loan', 'Avg_cash_loan']), 
-                            x='restgaeldinterval', y='value', color='isin', facet_col='variable', barmode='group',
-                            title='Average Loan Sizes Across Restgaeldinterval for Obl and Cash loans')
-    
-    st.plotly_chart(fig_loan_size)
-    
-    # Plotting across interval per gruppe
-    loan_size_df_gruppe_combined = calculate_avg_loan_size_per_laan_gruppe(df, selected_isins)
-    fig_loan_size = px.bar(
-        loan_size_df_gruppe_combined.melt(id_vars=['isin', 'restgaeldinterval', 'laan_gruppe'], value_vars=['avg_loan_size']),
-        x='restgaeldinterval',
-        y='value',
-        color='isin',
-        facet_col='laan_gruppe',
-        barmode='group',
-        title='Average Loan Sizes Across Restgaeldinterval for Privat and Other Loans'
-    )
-
-    st.plotly_chart(fig_loan_size)
-
-# Calculate interval distribution and store in a variable
-interval_distribution_df = calculate_interval_distribution(df, selected_isins)
-interval_distribution_df.columns = ["0-200k", '200k-500k', '500k-1m', '1-3m', '3-10m', '10-50m', '+50m']
-
-# Calculate shares and store in a variable
-loan_shares_df = gather_loan_shares(df, selected_isins)
-
-# Merge the two DataFrames side by side
-merged_df = interval_distribution_df.merge(loan_shares_df, left_index=True, right_index=True)
-merged_df.columns = merged_df.columns.map(str)
-
-# Display the merged DataFrame using Streamlit
-if selected_isins:
-    st.subheader('Interval Distribution, A/B Shares, and Obl/Cash Shares per ISIN')
-    st.dataframe(merged_df)
-
-else:
-    st.write("Please select at least one ISIN.")
-    
-total_int_distribution = calculate_interval_distribution(df, isin_options)
-total_int_distribution.columns = ["0-200k", '200k-500k', '500k-1m', '1-3m', '3-10m', '10-50m', '+50m']
-
-# Calculate shares and store in a variable
-total_loan_shares_df = gather_loan_shares(df, isin_options)
-
-total_merged_df = total_int_distribution.merge(total_loan_shares_df, left_index=True, right_index=True)
-total_merged_df.columns = total_merged_df.columns.map(str)
-
-top_20_isins_for_50m = total_merged_df.sort_values('+50m', ascending=False).head(20)
-
-st.subheader('Top 20 ISIN sorted loans above 50mio')
-st.dataframe(top_20_isins_for_50m)
+if __name__ == "__main__":
+    main()
